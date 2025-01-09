fbd072aca17cf83662a54f1822fcb6f43ab7bafe
ViniciusMarcuS2i <marcusononato@gmail.com>
2025-01-09 17:58:17 -0400

    feat: add google fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,31 @@
 import { Slot } from "expo-router";
-
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
 import "@/global.css";
 import { GluestackUIProvider } from "@/src/components/ui/gluestack-ui-provider";
 import { StatusBar } from "react-native";
+import Loading from "@/src/components/loading";
 
 function Layout() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) return <Loading />;
+
   return (
     <GluestackUIProvider mode="light">
-      <StatusBar barStyle="light-content" translucent />
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Slot />
     </GluestackUIProvider>
   );
