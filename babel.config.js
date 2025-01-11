ef146d5e9e76e5c38cb31651d0e6ea0ce126dbb1
ViniciusMarcuS2i <marcusononato@gmail.com>
2025-01-11 19:50:17 -0400

    fix: react native reanimated version compatible version

--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,27 @@ module.exports = function (api) {
     api.cache(true);
 
     return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
-            root: ["./"],
-            alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
-            }
-        }], 'react-native-reanimated/plugin',]
+        presets: [
+            [
+                "babel-preset-expo",
+                {
+                    jsxImportSource: "nativewind",
+                },
+            ],
+            "nativewind/babel",
+        ],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    root: ["./"],
+                    alias: {
+                        "@": "./",
+                        "tailwind.config": "./tailwind.config.js",
+                    },
+                },
+            ],
+            "react-native-reanimated/plugin",
+        ],
     };
 };
